8225a7eff5be3bc91be334949622e912c6078ff8
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-04-02 11:47:44 +0700

    Input data mahasiswa

--- StrukturD/StrukturD.cpp
+++ StrukturD/StrukturD.cpp
@@ -4,9 +4,21 @@
 #include <iostream>
 using namespace std;
 
-struct mahasisw {
+struct mahasiswa {
 	string nim;
 	string nama;
 	string alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << " Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << " Alamat Mahasiswa :";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+}
